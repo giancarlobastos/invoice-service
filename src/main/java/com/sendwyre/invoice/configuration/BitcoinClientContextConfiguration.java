4306c768a1b57d7e292e8926c7a9be6e64994fcf
Giancarlo Fernandes <giancarlo.fernandes@goeuro.com>
2019-05-03 00:02:59 -0300

    Bitcoin client configuration using @Value properties.

--- src/main/java/com/sendwyre/invoice/configuration/BitcoinClientContextConfiguration.java
+++ src/main/java/com/sendwyre/invoice/configuration/BitcoinClientContextConfiguration.java
@@ -3,6 +3,7 @@ package com.sendwyre.invoice.configuration;
 import org.bitcoinj.core.NetworkParameters;
 import org.bitcoinj.kits.WalletAppKit;
 import org.bitcoinj.params.TestNet3Params;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,13 +12,19 @@ import java.io.File;
 @Configuration
 public class BitcoinClientContextConfiguration {
 
+    @Value("${bitcoin-client.storage-folder}")
+    private String storageFolder;
+
+    @Value("${bitcoin-client.file-prefix}")
+    private String filePrefix;
+
     @Bean
     public NetworkParameters networkParameters() {
         return TestNet3Params.get();
     }
 
     @Bean
-    public WalletAppKit walletAppKit(NetworkParameters networkParameters, BitcoinClientConfigurationProperties configurationProperties) {
-        return new WalletAppKit(networkParameters, new File(configurationProperties.getStorageFolder()), configurationProperties.getFilePrefix());
+    public WalletAppKit walletAppKit(NetworkParameters networkParameters) {
+        return new WalletAppKit(networkParameters, new File(storageFolder), filePrefix);
     }
 }
