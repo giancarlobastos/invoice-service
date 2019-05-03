fceaf4b09ccd22ee527eb9b4bd62ce1937948b63
Giancarlo Fernandes <giancarlo.fernandes@goeuro.com>
2019-05-02 23:57:54 -0300

    Fixed a typo

--- src/main/java/com/sendwyre/invoice/configuration/BitcoinClientContextConfiguration.java
+++ src/main/java/com/sendwyre/invoice/configuration/BitcoinClientContextConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import java.io.File;
 
 @Configuration
-public class BitcointClientContextConfiguration {
+public class BitcoinClientContextConfiguration {
 
     @Bean
     public NetworkParameters networkParameters() {
